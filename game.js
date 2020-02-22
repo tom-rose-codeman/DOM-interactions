612ae7503d8a1ee5b7458c7a6291f0cef18fdbcc
tom-rose-codeman <tomrlrose@gmail.com>
2020-02-22 18:04:20 +1300

    add hide funct

--- game.js
+++ game.js
@@ -30,7 +30,11 @@ function makeBlue (evt) {
   updateCounts()
 }
 // CREATE FUNCTION hide HERE
-
+function hide (evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('hide')
+  updateCounts()
+}
 function updateCounts () {
   var totals = {
     blue: 0,
