cc7514c15cff32c7db7a8449af24598d30ac881e
tom-rose-codeman <tomrlrose@gmail.com>
2020-02-22 17:54:51 +1300

    add makeBlue funct

--- game.js
+++ game.js
@@ -22,7 +22,11 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+function makeBlue (evt) {
+  evt.preventDefault()
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
 
 function updateCounts () {
